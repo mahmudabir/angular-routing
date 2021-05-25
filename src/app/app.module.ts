cfca13488a3b04cd04e5fa2951e77a869f39febd
Abir Mahmud <amabirmahmud@gmail.com>
2021-05-25 17:55:59 +0600

    added all components, routes, auth.service, logged-in.guard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,75 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+
+import {
+  routes as childRoutes,
+  ProductsModule
+} from './products/products.module';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { ProtectedComponent } from './protected/protected.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+import {RouterModule, Routes} from '@angular/router';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import {ProductsComponent} from './products/products.component';
+import {LoggedInGuard} from './logged-in.guard';
+import {AUTH_PROVIDERS} from './auth.service';
+import {HashLocationStrategy, LocationStrategy} from '@angular/common';
+import {BrowserModule} from '@angular/platform-browser';
+
+
+const routes: Routes = [
+  // basic routes
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'contactus', redirectTo: 'contact' },
+
+  // authentication demo
+  { path: 'login', component: LoginComponent },
+  {
+    path: 'protected',
+    component: ProtectedComponent,
+    canActivate: [ LoggedInGuard ]
+  },
+
+  // nested
+  {
+    path: 'products',
+    component: ProductsComponent,
+    children: childRoutes
+  }
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContactComponent,
+    AboutComponent,
+    LoginComponent,
+    ProtectedComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes), // <-- routes
+
+    // added this for our child module
+    ProductsModule
   ],
-  providers: [],
+  providers: [
+    // uncomment this for "hash-bang" routing
+    // { provide: LocationStrategy, useClass: HashLocationStrategy },
+    AUTH_PROVIDERS,
+    LoggedInGuard
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/logged-in.guard.ts
@@ -0,0 +1,20 @@
+import {Injectable} from '@angular/core';
+import {ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree} from '@angular/router';
+import {Observable} from 'rxjs';
+import {AuthService} from './auth.service';
+
+
+@Injectable()
+export class LoggedInGuard implements CanActivate {
+  constructor(private authService: AuthService) {}
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+
+    const isLoggedIn = this.authService.isLoggedIn();
+    console.log('canActivate', isLoggedIn);
+    return isLoggedIn;
+  }
+
+}
