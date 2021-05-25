41795445299a4bd6c8b302f32907f53f062fd80b
Abir Mahmud <amabirmahmud@gmail.com>
2021-05-25 17:26:34 +0600

    products component & it's sub components added

---
+++ src/app/products/products.module.ts
@@ -0,0 +1,36 @@
+import {RouterModule, Routes} from '@angular/router';
+import {MainComponent} from './main/main.component';
+import {MoreInfoComponent} from './more-info/more-info.component';
+import {ProductComponent} from './product/product.component';
+import {NgModule} from '@angular/core';
+import {ProductsComponent} from './products.component';
+import {CommonModule} from '@angular/common';
+
+export const routes: Routes = [
+  {path: '', redirectTo: 'main', pathMatch: 'full'},
+  {path: 'main', component: MainComponent},
+  {path: 'more-info', component: MoreInfoComponent},
+  {path: ':id', component: ProductComponent},
+];
+
+@NgModule({
+  declarations: [
+    ProductsComponent,
+    ProductComponent,
+    MainComponent,
+    MoreInfoComponent
+  ],
+  exports: [
+    ProductsComponent,
+    ProductComponent,
+    MainComponent,
+    MoreInfoComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule
+  ],
+  providers: [],
+  bootstrap: []
+})
+export class ProductsModule {}
